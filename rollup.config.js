6337d4d827acbdb0af114eed778c33cfe7408d2c
ysuzuki19 <ysuzuki.cpp@gmail.com>
2021-09-26 05:30:17 +0900

    refact rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,52 +5,52 @@ import { terser } from 'rollup-plugin-terser';
 
 const entry = 'lib/index.ts';
 
-export default [
-  {
-    input: entry,
-    output: {
-      file: 'dist/index.es.js',
-      format: 'es',
-    },
-    external: ['react', 'recoil'],
-    plugins: [
-      typescript(),
-      babel({
-        extensions: ['.ts'],
-      }),
-    ],
+const es_config = {
+  input: entry,
+  output: {
+    file: 'dist/index.es.js',
+    format: 'es',
   },
+  external: ['react', 'recoil'],
+  plugins: [
+    typescript(),
+    babel({
+      extensions: ['.ts'],
+    }),
+  ],
+};
 
-  {
-    input: entry,
-    output: {
-      file: 'dist/index.d.ts',
-      format: 'es',
-    },
-    plugins: [dts()],
+const dts_config = {
+  input: entry,
+  output: {
+    file: 'dist/index.d.ts',
+    format: 'es',
   },
+  plugins: [dts()],
+};
 
-  {
-    input: entry,
-    output: {
-      file: 'dist/index.umd.min.js',
-      format: 'umd',
-      name: 'StructuredState',
-      exports: 'named',
-      indent: false,
-      globals: {
-        react: 'react',
-        recoil: 'recoil',
-      },
+const umd_config = {
+  input: entry,
+  output: {
+    file: 'dist/index.umd.min.js',
+    format: 'umd',
+    name: 'StructuredState',
+    exports: 'named',
+    indent: false,
+    globals: {
+      react: 'react',
+      recoil: 'recoil',
     },
-    external: ['react', 'recoil'],
-    plugins: [
-      typescript(),
-      babel({
-        extensions: ['.ts'],
-        exclude: 'node_modules/**',
-      }),
-      terser(),
-    ],
   },
-];
+  external: ['react', 'recoil'],
+  plugins: [
+    typescript(),
+    babel({
+      extensions: ['.ts'],
+      exclude: 'node_modules/**',
+    }),
+    terser(),
+  ],
+};
+
+export default [es_config, dts_config, umd_config];
